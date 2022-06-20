0ba6b4857762d389e88aa8a53bde1b9a28c11328
JosAngara <jmeh1992@gmail.com>
2022-06-20 08:16:55 -0500

    Added StatsGraph, it can graph but can't accept champeons already

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import { useState } from 'react';
 import "./styles.scss";
 import ChampionSelector from "./components/ChampionSelector";
+import StatsGraph from "./components/StatsGraph"
 
 function App() {
   let [search, setSearch] = useState("");
+  let [champions, setChampions] = useState([]);
+
+  const handleClick = (event) => {
+    setChampions([...champions, event.target.key])
+  }
+
   return (
     <div>
       <input type="text" value={search} onChange={(event) => setSearch(event.target.value)} />
       <ChampionSelector search={search} />
+      <StatsGraph searchedChampions={champions} stat="effectivehparmor" />
+      <StatsGraph searchedChampions={champions} stat="attackdamage" />
     </div>
   );
 }
